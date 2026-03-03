b727220375985032546d08da367f68317d41e2ce
Adan Siqueira <adan_siqueira@live.com>
2026-03-03 14:06:11 -0300

    feat: implement Timescale DB as database. Adapt models, session and config accordingly.

--- src/api/db/session.py
+++ src/api/db/session.py
@@ -1,15 +1,19 @@
 import sqlmodel
-from sqlmodel import SQLModel, Field, Session
+from sqlmodel import SQLModel, Session
 from sqlalchemy import text
-from .config import DATABASE_URL
+import timescaledb
 
-engine = sqlmodel.create_engine(DATABASE_URL, echo=True)
+from .config import DATABASE_URL, DB_TIMEZONE
+
+engine = timescaledb.create_engine(DATABASE_URL, echo=True, timezone=DB_TIMEZONE)
 
 def init_db():
     with engine.begin() as conn:
         conn.execute(text("SELECT pg_advisory_lock(1)"))
         SQLModel.metadata.create_all(engine)
         conn.execute(text("SELECT pg_advisory_unlock(1)"))
+        print('creating hypertables')
+        timescaledb.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
--- src/api/events/models.py
+++ src/api/events/models.py
@@ -3,24 +3,23 @@ from sqlmodel import SQLModel, Field
 from typing import List
 from datetime import datetime, timezone
 import sqlmodel
+from timescaledb import TimescaleModel
+from timescaledb.utils import get_utc_now
 
-def get_utc_now():
-    return datetime.now(timezone.utc)
+# page visits at any given time
+class EventModel(TimescaleModel, table=True):
+    # TimescaleModel already gives: 'id' and 'created_at'.
 
-class EventModel(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    page: str | None = None
+    page: str = Field (index=True)
     description: str | None = Field(default=None, max_length=255)
-    created_at: datetime = Field(
-        default_factory=get_utc_now,
-        sa_type=sqlmodel.DateTime(timezone=True),
-        nullable=False
-    )
     updated_at: datetime = Field(
         default_factory=get_utc_now,
         sa_type=sqlmodel.DateTime(timezone=True),
         nullable=False
     )
+
+    __chunk_time_interval__ = "INTERVAL 1 day"
+    __drop_after__ = "INTERVAL 3 months"
 
 class EventCreateSchema(SQLModel):
     page: str
